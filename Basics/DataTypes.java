60b9dedecbd8d7595f3e570e96b78154a64076c4
deepak <dkumar6@ciena.com>
2026-01-19 10:57:43 +0530

    First Program

---
+++ Basics/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        String str="Rahul Batra Sir";
+        char ch='x';
+        float avg=10.5f;
+        double rate=7.23456;
+        long num=10000000000890867l;
+        boolean isPass=true;
+        
+    }
+}
