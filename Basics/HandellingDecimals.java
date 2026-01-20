2ce94197a261363057e195521ef42bfa6d134a98
deepak <dkumar6@ciena.com>
2026-01-20 18:00:54 +0530

    basic folder restructure

--- Basics/HandellingDecimals.java
+++
@@ -1,9 +0,0 @@
-public class HandellingDecimals {
-    public static void main(String args[]){
-        //decimals needs to be handelled carefully
-        //should avoid === operator for decimal comparisons due to precision issues
-
-        //the Epsilon(Threshold Method)
-        
-    }
-}
