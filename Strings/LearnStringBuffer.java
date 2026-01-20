8ced6b4f053e366bfe234f06ec37f6a127051de3
deepak <dkumar6@ciena.com>
2026-01-20 18:22:29 +0530

    String Mutaion Study

--- Strings/LearnStringBuffer.java
+++ Strings/LearnStringBuffer.java
@@ -1,5 +1,67 @@
 package Strings;
 
 public class LearnStringBuffer {
-    
+    public static void main(String args[]) {
+        // Creating StringBuffer
+        StringBuffer s = new StringBuffer();
+
+        // Adding elements in StringBuffer
+        s.append("Hello");
+        s.append(" ");
+        s.append("world");
+
+        // String with the StringBuffer value
+        String str = s.toString();
+        System.out.println(str);
+
+        // 1. Using default constructor
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("Hello");
+        System.out.println("Default Constructor: " + sb1);
+
+        // 2. Using constructor with specified capacity
+        StringBuffer sb2 = new StringBuffer(50);
+        sb2.append("Java Programming");
+        System.out.println("With Capacity 50: " + sb2);
+
+        // 3. Using constructor with String
+        StringBuffer sb3 = new StringBuffer("Welcome");
+        sb3.append(" to Java");
+        System.out.println("With String: " + sb3);
+
+        // StringBuffer Methods
+        StringBuffer sb = new StringBuffer("Java is Fun");
+        sb.insert(1, "Java");
+
+        // Now original string is changed
+        System.out.println(sb);
+
+        sb.replace(1, 3, "Java");
+        System.out.println(sb);
+
+        sb.delete(1, 3);
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+
+        // default 16
+        System.out.println(sb.capacity());
+        sb.append("Hello");
+
+        // now 16
+        System.out.println(sb.capacity());
+        sb.append("java is my favourite language");
+
+        // (oldcapacity*2)+2
+        System.out.println(sb.capacity());
+
+        int p = sb.length();
+
+        // Getting the capacity of the string
+        System.out.println("Length of stringbuffer = " + p);
+
+    }
 }
+
+// ThreadSafe StringBuilder
--- Strings/LearnStringBuilder.java
+++ Strings/LearnStringBuilder.java
@@ -1,7 +1,69 @@
 package Strings;
 
 public class LearnStringBuilder {
-    public static void main(String args[]){
-        
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("Learning Java");
+        System.out.println("Initial StringBuilder: " + sb);
+
+        sb.append(" is awesome!");
+        System.out.println("After append: " + sb);
+
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("Hello");
+        System.out.println("sb1: " + sb1);
+
+        StringBuilder sb2 = new StringBuilder(50);
+        sb2.append("This has initial capacity 50");
+        System.out.println("sb2: " + sb2);
+
+        StringBuilder sb3 = new StringBuilder("Deepak");
+        sb3.append("HereForFun");
+        System.out.println("sb3: " + sb3);
+
+        CharSequence cs = "Java";
+        StringBuilder sb4 = new StringBuilder(cs);
+        sb4.append("Programming");
+        System.out.println("sb4: " + sb4);
+
+        // Commonly Used Methods
+        sb.insert(13, " Java");
+        System.out.println("After insert: " + sb);
+
+        sb.replace(0, 5, "Welcome to");
+        System.out.println("After replace: " + sb);
+
+        sb.delete(8, 14);
+        System.out.println("After delete: " + sb);
+
+        sb.reverse();
+        System.out.println("After reverse: " + sb);
+
+        System.out.println("Capacity: " + sb.capacity());
+        System.out.println("Length: " + sb.length());
+
+        char c = sb.charAt(5);
+        System.out.println("Character at index 5: " + c);
+
+        sb.setCharAt(5, 'X');
+        System.out.println("After setCharAt: " + sb);
+
+        String sub = sb.substring(5, 10);
+        System.out.println("Substring (5-10): " + sub);
+
+        sb.reverse(); // Revert for search
+        System.out.println("Index of 'Geeks': " + sb.indexOf("Geeks"));
+
+        sb.deleteCharAt(5);
+        System.out.println("After deleteCharAt: " + sb);
+
+        String result = sb.toString();
+        System.out.println("Final String: " + result);
+
     }
 }
+
+// It provides similar functionality to StringBuffer, but without thread safety.
+// StringBuilder is not synchronized, so it performs better in single-threaded
+// applications.
+// Use StringBuffer only when thread safety is required; otherwise, prefer
+// StringBuilder for improved performance.
